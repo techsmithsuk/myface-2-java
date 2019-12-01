b478b8baf3298eb7a03c1a4a84f76302225f2115
Mike <Walker>
2019-12-01 21:16:49 +0000

    Add Create User Endpoint.

---
+++ src/main/java/uk/co/techswitch/myface/controllers/UsersController.java
@@ -0,0 +1,27 @@
+package uk.co.techswitch.myface.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import uk.co.techswitch.myface.models.api.CreateUser;
+import uk.co.techswitch.myface.services.UsersService;
+
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+    private final UsersService usersService;
+
+    @Autowired
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public ModelAndView createUser(@ModelAttribute CreateUser user) {
+        usersService.createUser(user);
+        return new ModelAndView("index");
+    }
+}
