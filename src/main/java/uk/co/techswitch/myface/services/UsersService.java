3f866430f476ccf386e8a36059d5d7d0d06c2752
Mike <Walker>
2019-12-01 20:54:59 +0000

    Added Create User Service

--- src/main/java/uk/co/techswitch/myface/services/UsersService.java
+++ src/main/java/uk/co/techswitch/myface/services/UsersService.java
@@ -1,4 +1,27 @@
 package uk.co.techswitch.myface.services;
 
-public class UsersService {
+import org.springframework.stereotype.Service;
+import uk.co.techswitch.myface.models.api.CreateUser;
+
+@Service
+public class UsersService extends DatabaseService {
+
+    public long createUser(CreateUser user) {
+        return jdbi.withHandle(handle -> {
+            handle.createUpdate(
+                    "INSERT INTO users " +
+                            "(username, email, first_name, last_name) " +
+                            "VALUES " +
+                            "(:username, :email, :firstName, :lastName)")
+                    .bind("username", user.getUsername())
+                    .bind("email", user.getEmail())
+                    .bind("firstName", user.getFirstName())
+                    .bind("lastName", user.getLastName())
+                    .execute();
+
+            return handle.createQuery("SELECT LAST_INSERT_ID()")
+                    .mapTo(Long.class)
+                    .one();
+        });
+    }
 }
